1b276ad4eda203aca3c43da4f86a25d88713f175
liushurui666 <675948133@qq.com>
2020-09-04 16:01:57 +0800

    master

---
+++ ooo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class ooo extends Component {
+  render() {
+    return (
+      <div>
+        这是所狗
+      </div>
+    )
+  }
+}
+
+export default ooo

11524f5567e0fa8bd3be76d9567fb5a346c3216c
liushurui666 <675948133@qq.com>
2020-09-04 15:36:32 +0800

    下拉加载效果

--- xxx.js
+++ xxx.js
@@ -1,1 +1,13 @@
-// 把文件夹托管到git
+// 1.把文件夹托管到git
+// 2.git add .
+// 3.git commit -m 'master'
+// 4.git remote add origin https://github.com/liushurui666/root.git
+//拷贝自己的这行代码 创建git仓库
+
+
+
+
+// 正常进公司以后步骤
+// 配置git
+//  git config --global user.name 'github的用户名'
+// git config --global user.email 'github登录邮箱'

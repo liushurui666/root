fcb71bb3e5fdb4c053bba60033e10768833fc5e6
liushurui666 <675948133@qq.com>
2020-09-04 15:59:26 +0800

    master

--- xxx.js
+++ xxx.js
@@ -11,3 +11,12 @@
 // 配置git
 //  git config --global user.name 'github的用户名'
 // git config --global user.email 'github登录邮箱'
+// 第一步 首先需要创建公钥
+// ssh-keygen -t rsa  -C '你的github的邮箱'
+// 第二步打开公钥
+// cat ~
+// 常用命令
+//  git status 查看那些文件修改了
+// git commit -m 'master': 提交代码
+// git push origin master 往远程分支提交代码  
+111121312312123123123
